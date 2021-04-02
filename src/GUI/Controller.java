4cac57af35ee73b3ccf3da0c1a63d17bfb2846e2
Anton <antzotik@gmail.com>
2021-04-02 03:51:55 +0300

    Login page v1

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    private boolean isLogin;
     final private ArrayList<XYChart.Series> list = new ArrayList<>();
     private int count=0;
     @FXML
--- src/GUI/LoginController.java
+++ src/GUI/LoginController.java
@@ -1,11 +1,23 @@
 package GUI;
 
+import com.sun.source.tree.TryTree;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginController {
-
     /**
      * Login text field
      */
@@ -15,7 +27,31 @@ public class LoginController {
      * Password text field
      */
     @FXML
-    private TextField PasswordTextField;
+    private PasswordField HidePasswordTextField;
+
+    @FXML
+    private TextField ShowPasswordTextField;
+
+    @FXML
+    public Button LoginButtom;
+
+    @FXML
+    private CheckBox ToggleButtom;
+
+    @FXML
+    void TogglePasAnAction(ActionEvent event){
+        if(ToggleButtom.isSelected()){
+            ShowPasswordTextField.setText(HidePasswordTextField.getText());
+            ShowPasswordTextField.setVisible(true);
+            HidePasswordTextField.setVisible(false);
+            return;
+        }
+        else{
+            HidePasswordTextField.setText(ShowPasswordTextField.getText());
+            HidePasswordTextField.setVisible(true);
+            ShowPasswordTextField.setVisible(false);
+        }
+    }
 
     /**
      * Data entry buttom
@@ -24,7 +60,26 @@ public class LoginController {
     @FXML
     void LoginButtomAnAction(ActionEvent event) {
         System.out.println(LoginTextField.getText());
-
+        //Код смены окон
+        /*
+        if(если в бд есть пользователь){
+            try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Resources/CryptMain.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("My New Stage Title");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root));
+            TimeUnit.SECONDS.sleep(2);
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            //Попробуйте залогиниться снова
+        }
+        */
     }
 
     /**
